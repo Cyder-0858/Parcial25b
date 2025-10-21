867a217936b40d963d810a4cff95b65bb4ae65a5
Cyder-0858 <shiyu.huang@usp.ceu.es>
2025-10-21 07:52:06 +0000

    1

--- Ejercicio_1/Ej1.py
+++ Ejercicio_1/Ej1.py
@@ -1,16 +1,16 @@
 #Que es la recursividad. Que elementos se deben cumplir para que un algoritmo recursivo sea correcto.
 #Escribe una función recursiva que devuelva la suma del valor absoluto de una lista de números. Escribe un código de prueba con una llamada a la función.
 
-numeros = [0,1,2,3,4,5,6,7,8,9]
-
-def suma_va(lista, i=0):
+def suma_valor_absoluto(lista, i=0):
+    # Caso base: si ya recorrimos toda la lista
     if i == len(lista):
         return 0
     else:
-        resultado = (lista[i]**2)**0.5 + suma_va(lista, i + 1)
-        return resultado
+        return abs(lista[i]) + suma_valor_absoluto(lista, i + 1)
 
-prueba1 = suma_va(numeros)
-print(prueba1)
+# --- Código de prueba ---
+numeros = [-3, 5, -2, 7, -1]
+resultado = suma_valor_absoluto(numeros)
+print(f"La suma del valor absoluto de {numeros} es {resultado}")
 
 
